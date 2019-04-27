f59c1891cd71c1a1915871bbb0cad6168cfc4163
Vidhir Kaup <vk4web@gmail.com>
2019-04-27 18:08:10 -0400

    added polymorphic behaviour - using constructor

--- src/main/java/com/virtual/labs/springbasics/SpringBasicsApplication.java
+++ src/main/java/com/virtual/labs/springbasics/SpringBasicsApplication.java
@@ -1,6 +1,8 @@
 package com.virtual.labs.springbasics;
 
 import com.virtual.labs.springbasics.search.BinarySearchImpl;
+import com.virtual.labs.springbasics.search.BubbleSortAlgorithm;
+import com.virtual.labs.springbasics.search.QuickSortAlgorithm;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,7 +10,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringBasicsApplication {
 
     public static void main(String[] args) {
-        BinarySearchImpl binarySearch = new BinarySearchImpl();
+        BinarySearchImpl binarySearch = new BinarySearchImpl(new QuickSortAlgorithm());
         int result = binarySearch.binarySearch(new int[]{12, 4, 6}, 5);
         System.out.println("Search result = " + result);
 
--- src/main/java/com/virtual/labs/springbasics/search/BinarySearchImpl.java
+++ src/main/java/com/virtual/labs/springbasics/search/BinarySearchImpl.java
@@ -2,12 +2,14 @@ package com.virtual.labs.springbasics.search;
 
 public class BinarySearchImpl {
 
+    private SortAlgorithm sortAlgorithm;
+
+    public BinarySearchImpl(SortAlgorithm sortAlgorithm) {
+        this.sortAlgorithm = sortAlgorithm;
+    }
+
     public int binarySearch(int[] numbers, int numberToSearchFor){
         // sort numbers
-        SortAlgorithm sortAlgorithm = new BubbleSortAlgorithm();
-        sortAlgorithm.sort(numbers);
-
-        sortAlgorithm = new QuickSortAlgorithm();
         sortAlgorithm.sort(numbers);
 
         // search the number
