21ebaec9c7432354cda379a1a0827ca99e27cc56
Vidhir Kaup <vk4web@gmail.com>
2019-04-28 10:25:55 -0400

    added a simple REST service (/books)

---
+++ src/main/java/com/virtual/labs/springbasics/books/Book.java
@@ -0,0 +1,37 @@
+package com.virtual.labs.springbasics.books;
+
+public class Book {
+
+    private int id;
+
+    private String name;
+
+    private String author;
+
+    public Book(int id, String name, String author) {
+        this.id = id;
+        this.name = name;
+        this.author = author;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", author='" + author + '\'' +
+                '}';
+    }
+}
---
+++ src/main/java/com/virtual/labs/springbasics/books/BooksController.java
@@ -0,0 +1,19 @@
+package com.virtual.labs.springbasics.books;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
+
+@RestController
+public class BooksController {
+    @GetMapping("/books")
+    public List<Book> getAllBooks(){
+        return Arrays.asList(
+                new Book(1, "Spring In Action", "Josh Long"),
+                new Book(2, "Learning Java 8", "Expert Java")
+        );
+    }
+
+}
