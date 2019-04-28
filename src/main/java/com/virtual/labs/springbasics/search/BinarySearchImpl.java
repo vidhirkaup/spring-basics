d6b301a0195b5e74659653a17dfc8eddc15a59f4
Vidhir Kaup <vk4web@gmail.com>
2019-04-28 07:35:42 -0400

    replaced constructor injection with Autowired injection

--- src/main/java/com/virtual/labs/springbasics/search/BinarySearchImpl.java
+++ src/main/java/com/virtual/labs/springbasics/search/BinarySearchImpl.java
@@ -9,10 +9,6 @@ public class BinarySearchImpl {
     @Autowired
     private SortAlgorithm sortAlgorithm;
 
-    public BinarySearchImpl(SortAlgorithm sortAlgorithm) {
-        this.sortAlgorithm = sortAlgorithm;
-    }
-
     public int search(int[] numbers, int numberToSearchFor){
         // sort numbers
         sortAlgorithm.sort(numbers);
