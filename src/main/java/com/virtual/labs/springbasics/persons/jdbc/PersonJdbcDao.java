e4a98703534fd4e5b80969130989b0ca608956c3
Vidhir Kaup <vk4web@gmail.com>
2019-04-28 17:34:46 -0400

    using Spring JDBC - implemented Custom RowMapper for Person

--- src/main/java/com/virtual/labs/springbasics/persons/jdbc/PersonJdbcDao.java
+++ src/main/java/com/virtual/labs/springbasics/persons/jdbc/PersonJdbcDao.java
@@ -4,8 +4,11 @@ import com.virtual.labs.springbasics.persons.entity.Person;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.sql.Timestamp;
 import java.util.List;
 
@@ -15,27 +18,40 @@ public class PersonJdbcDao {
     @Autowired
     JdbcTemplate jdbcTemplate;
 
+    private class PersonRowMapper implements RowMapper<Person> {
+
+        @Override
+        public Person mapRow(ResultSet resultSet, int i) throws SQLException {
+            Person person = new Person();
+            person.setId(resultSet.getInt("id"));
+            person.setName(resultSet.getString("name"));
+            person.setLocation(resultSet.getString("location"));
+            person.setBirthDate(resultSet.getTimestamp("birth_date"));
+            return person;
+        }
+    }
+
     public List<Person> findAll() {
         return jdbcTemplate.query("select * from person",
-                new BeanPropertyRowMapper<Person>(Person.class));
+                new PersonRowMapper());
     }
 
     public Person findById(int id) {
         return jdbcTemplate.queryForObject("select * from person where id=?",
                 new Object[]{id},
-                new BeanPropertyRowMapper<Person>(Person.class));
+                new BeanPropertyRowMapper<>(Person.class));
     }
 
     public List<Person> findByName(String name) {
         return jdbcTemplate.query("select * from person where name=?",
                 new Object[] {name},
-                new BeanPropertyRowMapper<Person>(Person.class));
+                new BeanPropertyRowMapper<>(Person.class));
     }
 
     public List<Person> findByLocation(String location) {
         return jdbcTemplate.query("select * from person where location=?",
                 new Object[] {location},
-                new BeanPropertyRowMapper<Person>(Person.class));
+                new BeanPropertyRowMapper<>(Person.class));
     }
 
     public int deleteById(int id) {
@@ -56,5 +72,4 @@ public class PersonJdbcDao {
         return jdbcTemplate.update("update person set name = ?, location = ?, birth_date = ? where id = ?",
                 new Object[]{person.getName(), person.getLocation(), new Timestamp(person.getBirthDate().getTime()), person.getId()});
     }
-
 }
